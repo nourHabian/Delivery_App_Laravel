f848bb5c5df06fc57ccf1172376096e6a45820ec
hajar <hajar9975211@gmail.com>
2024-12-06 12:52:17 +0300

    test3

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
+//test1
 Route::post('user/register', [UserController::class, 'register']);
 Route::post('user/login', [UserController::class, 'login']);
+//test2
